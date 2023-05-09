f51b6b9b11827f9fd719e2f51b764aad266d848b
sonika <sonikak004@gmail.com>
2023-05-09 05:07:18 +0100

    Reconstruct UI class to arrow function

--- Modules/ui.js
+++ Modules/ui.js
@@ -1,41 +1,43 @@
 import Store from './store.js';
 
-export default class UI {
-  static displayBooks() {
-    const books = Store.getBooks();
-    books.forEach((book) => UI.addBookToList(book));
-  }
+const UI = () => {};
 
-  static addBookToList(book) {
-    const list = document.querySelector('#book-list');
-    const row = document.createElement('tr');
-    row.innerHTML = `
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td><a href="#" class="btn btn-secondary btn-sm delete btn-hover">Remove</a></td>
-    `;
-    list.appendChild(row);
-  }
+UI.displayBooks = () => {
+  const books = Store.getBooks();
+  books.forEach((book) => UI.addBookToList(book));
+};
 
-  static deleteBook(el) {
-    if (el.classList.contains('delete')) {
-      el.parentElement.parentElement.remove();
-    }
-  }
+UI.addBookToList = (book) => {
+  const list = document.querySelector('#book-list');
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td><a href="#" class="btn btn-secondary btn-sm delete btn-hover">Remove</a></td>
+  `;
+  list.appendChild(row);
+};
 
-  static showAlert(message, className) {
-    const div = document.createElement('div');
-    div.className = `alert alert-${className}`;
-    div.appendChild(document.createTextNode(message));
-    const container = document.querySelector('.container');
-    const form = document.querySelector('#book-form');
-    container.insertBefore(div, form);
-    // Vanish in 2.5 seconds
-    setTimeout(() => document.querySelector('.alert').remove(), 2500);
+UI.deleteBook = (el) => {
+  if (el.classList.contains('delete')) {
+    el.parentElement.parentElement.remove();
   }
+};
 
-  static clearFields() {
-    document.querySelector('#title').value = '';
-    document.querySelector('#author').value = '';
-  }
-}
+UI.showAlert = (message, className) => {
+  const div = document.createElement('div');
+  div.className = `alert alert-${className}`;
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector('.container');
+  const form = document.querySelector('#book-form');
+  container.insertBefore(div, form);
+  // Vanish in 2.5 seconds
+  setTimeout(() => document.querySelector('.alert').remove(), 2500);
+};
+
+UI.clearFields = () => {
+  document.querySelector('#title').value = '';
+  document.querySelector('#author').value = '';
+};
+
+export default UI;
